c094665c6a9cb45d5fc24ce936e8b0a6d44ff0e6
Kanei <77195827+zbraa@users.noreply.github.com>
2022-01-12 21:13:20 +0900

    pluginまとめた

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -38,6 +38,14 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    {
+      src: '~/plugins/axios/index.js',
+      mode: "client"
+    },
+    {
+      src: '~/plugins/axios/client.js',
+      mode: "client"
+    }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
