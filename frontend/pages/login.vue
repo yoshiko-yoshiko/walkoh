6a8c4af91f1ca549db7c1d1b3e6d13e33c98b4fc
Keiichiro Sato <10kmartinis29@gmail.com>
2022-02-08 20:13:20 +0900

    Change layout to Two Columns and tweak for login page

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,86 +1,76 @@
 <template>
-  <div
-    class="h-screen bg-gradient-to-br bg-primary-sub flex justify-center items-center w-full"
-  >
-    <div class="py-6 sm:py-8 lg:py-12">
-      <div class="max-w-screen-xl px-4 md:px-8 mx-auto">
-        <div class="grid md:grid-cols-2 gap-8 lg:gap-12">
-          <div>
-            <div class="h-64 md:h-auto bg-gray-100 overflow-hidden rounded-lg">
-              <img
-                src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=600&h=750"
-                loading="lazy"
-                alt="Photo by Martin Sanchez"
-                class="w-full h-full object-cover object-center"
-              />
-            </div>
-          </div>
+  <div class="container mx-auto flex flex-wrap md:h-screen items-center">
+    <div class="w-full md:w-1/2 mt-20 md:mt-0">
+      <div class="md:w-96 mx-auto md:mr-10">
+        <img src="~/assets/images/login_img.png" class="h-28 mt-0 md:mt-0 w-full object-cover md:object-contain md:h-auto test">
+      </div>
+    </div>
+    <div class="w-full md:w-1/2 mt-20 md:mt-0">
+      <div class="flex justify-center items-center lg:justify-between">
+        <div class="md:mt-8">
+          <p class="text-primary-main font-bold text-left">
+            Sign Up
+          </p>
 
-          <div class="md:pt-8">
-            <p class="text-primary-main font-bold text-center md:text-left">
-              Sign up
-            </p>
+          <h1
+            class="text-black text-3xl md:text-2xl font-bold text-left mb-4 md:mb-6"
+          >
+            wolkohへようこそ！
+          </h1>
 
-            <h1
-              class="text-black text-2xl sm:text-3xl font-bold text-center md:text-left mb-4 md:mb-6"
-            >
-              wolkohへようこそ！
-            </h1>
-
-            <form @submit.prevent="login">
-              <div class="bg-white px-10 py-8 rounded-xl w-screen max-w-sm">
-                <div class="space-y-4">
-                  <div>
-                    <label
-                      for="email"
-                      class="block mb-1 text-black font-semibold"
-                      >ユーザー名</label
-                    >
-                    <input
-                      type="text"
-                      class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-                      v-model="auth.name"
-                    />
-                  </div>
-                  <div>
-                    <label
-                      for="email"
-                      class="block mb-1 text-black font-semibold"
-                      >メールアドレス</label
-                    >
-                    <input
-                      type="text"
-                      class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-                      v-model="auth.email"
-                    />
-                  </div>
-                  <div>
-                    <label
-                      for="email"
-                      class="block mb-1 text-black font-semibold"
-                      >パスワード</label
-                    >
-                    <input
-                      type="text"
-                      class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
-                      v-model="auth.password"
-                    />
-                  </div>
+          <form @submit.prevent="login">
+            <div class="bg-white py-8 rounded-xl max-w-xs">
+              <div class="space-y-4">
+                <div>
+                  <label for="email" class="block mb-1 text-black font-semibold"
+                    >ユーザー名</label
+                  >
+                  <input
+                    type="text"
+                    class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full border-2 border-indigo-50 focus:bg-white focus:border-primary-main"
+                    v-model="auth.name"
+                  />
                 </div>
-                <button
-                  class="mt-8 w-full bg-primary-main text-indigo-50 font-bold py-2 rounded-md text-lg tracking-wide"
-                  v-bind:disabled="processing"
-                >
-                  登録する
-                </button>
+                <div>
+                  <label for="email" class="block mb-1 text-black font-semibold"
+                    >メールアドレス</label
+                  >
+                  <input
+                    type="text"
+                    class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full border-2 border-indigo-50 focus:bg-white focus:border-primary-main"
+                    v-model="auth.email"
+                  />
+                </div>
+                <div>
+                  <label for="email" class="block mb-1 text-black font-semibold"
+                    >パスワード</label
+                  >
+                  <input
+                    type="text"
+                    class="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full border-2 border-indigo-50 focus:bg-white focus:border-primary-main"
+                    v-model="auth.password"
+                  />
+                </div>
               </div>
-            </form>
-          </div>
+              <button
+                class="mt-8 w-full bg-primary-main text-indigo-50 font-bold py-2 rounded-md tracking-wide hover:brightness-110"
+                v-bind:disabled="processing"
+              >
+                登録する
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<style>
+  .test {
+    object-position: 30% 35%
+  }
+</style>
 
 <script>
 export default {
