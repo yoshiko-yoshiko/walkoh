1583341af1ab242a830be5de307c7d43dd3b8192
Keiichiro Sato <10kmartinis29@gmail.com>
2022-02-26 18:18:51 +0900

    Add keyboard control to swiper

--- frontend/pages/swiper.vue
+++ frontend/pages/swiper.vue
@@ -1,64 +1,56 @@
 <template>
-  <div class="example">
-    <!-- component: only render Swiper on browser env -->
+<div class="example">
     <client-only>
-      <swiper
-        ref="carousel"
-        class="swiper"
-        :options="swiperOptions"
-        @ready="onSwiperRedied"
-        @clickSlide="onSwiperClickSlide"
-        @slideChangeTransitionStart="onSwiperSlideChangeTransitionStart"
-      >
+      <swiper class="swiper" :options="swiperOption" @ready="onSwiperRedied"
+        @clickSlide="onSwiperClickSlide">
         <swiper-slide>Slide 1</swiper-slide>
         <swiper-slide>Slide 2</swiper-slide>
         <swiper-slide>Slide 3</swiper-slide>
         <swiper-slide>Slide 4</swiper-slide>
         <swiper-slide>Slide 5</swiper-slide>
         <swiper-slide>Slide 6</swiper-slide>
-        <div slot="pagination" class="swiper-pagination" />
-      </swiper>
+        <div class="swiper-pagination" slot="pagination"></div>
+        <div class="swiper-button-prev" slot="button-prev"></div>
+        <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
     </client-only>
   </div>
 </template>
 
 <script>
+import 'swiper/css/swiper.css'
+
 export default {
   name: 'SwiperNuxt',
-  data () {
+  data() {
     return {
-      swiperOptions: {
-        loop: true,
-        slidesPerView: 'auto',
-        centeredSlides: true,
+      swiperOption: {
+        slidesPerView: 1,
         spaceBetween: 30,
+        keyboard: {
+          enabled: true,
+        },
         pagination: {
-          el: '.swiper-pagination',
-          dynamicBullets: true
-        }
-      }
-    }
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      },
+    };
   },
-  methods: {
-    onSwiperRedied (swiper) {
-      console.log('Swiper redied!', swiper)
-    },
-    onSwiperSlideChangeTransitionStart () {
-      console.log('SwiperSlideChangeTransitionStart!')
-    },
-    onSwiperClickSlide (index, reallyIndex) {
-      console.log('Swiper click slide!', reallyIndex)
-    }
-  }
-}
+};
 </script>
 
 <style lang="scss" scoped>
   .example {
     height: auto;
     .swiper {
+      margin-top: 5rem;
       height: 300px;
-      width: 100%;
+      width: 80%;
       .swiper-slide {
         text-align: center;
         font-size: 38px;
