a0e6c837dd4613ca330ceb63ef40dc0bf7596009
Keiichiro Sato <10kmartinis29@gmail.com>
2022-03-04 21:35:27 +0900

    Use centered slides and v-for

--- frontend/pages/swiper.vue
+++ frontend/pages/swiper.vue
@@ -1,37 +1,50 @@
 <template>
-<div class="example">
+  <div class="example">
     <client-only>
-      <swiper class="swiper" :options="swiperOption" @ready="onSwiperRedied"
-        @clickSlide="onSwiperClickSlide">
-        <swiper-slide>Slide 1</swiper-slide>
+      <swiper
+        class="swiper"
+        :options="swiperOption"
+        @ready="onSwiperRedied"
+        @clickSlide="onSwiperClickSlide"
+      >
+      <swiper-slide v-for="item in 9" :key="item">
+      <span>Slide-{{ item }}</span>
+      </swiper-slide>
+        <!-- <swiper-slide>Slide 1</swiper-slide>
         <swiper-slide>Slide 2</swiper-slide>
         <swiper-slide>Slide 3</swiper-slide>
         <swiper-slide>Slide 4</swiper-slide>
         <swiper-slide>Slide 5</swiper-slide>
         <swiper-slide>Slide 6</swiper-slide>
+        <swiper-slide>Slide 7</swiper-slide>
+        <swiper-slide>Slide 8</swiper-slide>
+        <swiper-slide>Slide 9</swiper-slide>
+        <swiper-slide>Slide 10</swiper-slide> -->
         <div class="swiper-pagination" slot="pagination"></div>
         <div class="swiper-button-prev" slot="button-prev"></div>
         <div class="swiper-button-next" slot="button-next"></div>
-        </swiper>
+      </swiper>
     </client-only>
   </div>
 </template>
 
 <script>
-import 'swiper/css/swiper.css'
+import "swiper/css/swiper.css";
 
 export default {
-  name: 'SwiperNuxt',
+  name: "SwiperNuxt",
   data() {
     return {
       swiperOption: {
-        slidesPerView: 1,
-        spaceBetween: 30,
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 300,
         keyboard: {
           enabled: true,
         },
         pagination: {
           el: ".swiper-pagination",
+          type: "progressbar",
           clickable: true,
         },
         navigation: {
@@ -45,26 +58,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .example {
-    height: auto;
-    .swiper {
-      margin-top: 5rem;
-      height: 300px;
-      width: 80%;
-      .swiper-slide {
-        text-align: center;
-        font-size: 38px;
-        font-weight: 700;
-        background-color: #eee;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      .swiper-pagination {
-        > .swiper-pagination-bullet {
-          background-color: red;
-        }
+.example {
+  height: auto;
+  .swiper {
+    margin-top: 5rem;
+    height: 500px;
+    width: 80%;
+    .swiper-slide:nth-child(2n) {
+      width: 60%;
+    }
+    .swiper-slide:nth-child(3n) {
+      width: 40%;
+    }
+    .swiper-slide {
+      text-align: center;
+      font-size: 38px;
+      font-weight: 700;
+      background-color: #eee;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+    .swiper-pagination {
+      > .swiper-pagination-bullet {
+        background-color: red;
       }
     }
   }
+}
 </style>
