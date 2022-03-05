350f51d1b52c2979137b6f4604c80ba24523905a
Keiichiro Sato <10kmartinis29@gmail.com>
2022-03-05 23:39:06 +0900

    Add work compornent

---
+++ frontend/components/work.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>
+      <strong class=" text-2xl">{{ $store.state.products.title }}</strong>
+    </div>
+    <div>
+      <p>
+        <span>
+          <b class=" text-xl">&yen; {{ $store.state.products.price }}</b>
+        </span>
+      </p>
+    </div>
+    <div>
+      <p>{{ $store.state.products.description }}</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
