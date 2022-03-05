b122553736a03785ca2f153c6ee52206dbf69651
Keiichiro Sato <10kmartinis29@gmail.com>
2022-03-05 23:48:02 +0900

    Use work and PrimaryButton compornent

--- frontend/pages/swiper.vue
+++ frontend/pages/swiper.vue
@@ -1,30 +1,28 @@
 <template>
-  <div class="example">
-    <client-only>
-      <swiper
-        class="swiper"
-        :options="swiperOption"
-        @ready="onSwiperRedied"
-        @clickSlide="onSwiperClickSlide"
-      >
-      <swiper-slide v-for="item in 9" :key="item">
-      <span>Slide-{{ item }}</span>
-      </swiper-slide>
-        <!-- <swiper-slide>Slide 1</swiper-slide>
-        <swiper-slide>Slide 2</swiper-slide>
-        <swiper-slide>Slide 3</swiper-slide>
-        <swiper-slide>Slide 4</swiper-slide>
-        <swiper-slide>Slide 5</swiper-slide>
-        <swiper-slide>Slide 6</swiper-slide>
-        <swiper-slide>Slide 7</swiper-slide>
-        <swiper-slide>Slide 8</swiper-slide>
-        <swiper-slide>Slide 9</swiper-slide>
-        <swiper-slide>Slide 10</swiper-slide> -->
-        <div class="swiper-pagination" slot="pagination"></div>
-        <div class="swiper-button-prev" slot="button-prev"></div>
-        <div class="swiper-button-next" slot="button-next"></div>
-      </swiper>
-    </client-only>
+  <div>
+    <div class="example">
+      <client-only>
+        <swiper
+          class="swiper"
+          :options="swiperOption"
+          @ready="onSwiperRedied"
+          @clickSlide="onSwiperClickSlide"
+        >
+          <swiper-slide v-for="item in 9" :key="item">
+            <span>Slide-{{ item }}</span>
+          </swiper-slide>
+          <div class="swiper-pagination" slot="pagination"></div>
+          <div class="swiper-button-prev" slot="button-prev"></div>
+          <div class="swiper-button-next" slot="button-next"></div>
+        </swiper>
+      </client-only>
+    </div>
+    <div class="flex justify-center">
+      <work class="mt-10" />
+    </div>
+    <div class="flex justify-center">
+      <PrimaryButton class="mt-10" />
+    </div>
   </div>
 </template>
 
@@ -84,7 +82,7 @@ export default {
     }
     .swiper-pagination-progressbar {
       background: #fff;
-      }
+    }
     // .swiper-button-prev {
     //   color: #4acea2;
     // }
