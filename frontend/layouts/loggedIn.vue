c387a14182079d7d79dcc0a88be93679675b943e
Keiichiro Sato <10kmartinis29@gmail.com>
2022-03-07 02:04:23 +0900

    Add logged-in layout

---
+++ frontend/layouts/loggedIn.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Header from "../components/TheHeader.vue";
+export default {
+  components: {
+    Header,
+  },
+  head: {
+    bodyAttrs: {
+      class: "bg-white",
+    },
+  },
+};
+</script>
