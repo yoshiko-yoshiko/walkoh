e97e7a6c5e4746b54c93ea9ccaa0bb0489eeab32
Keiichiro Sato <10kmartinis29@gmail.com>
2022-03-07 02:06:41 +0900

    Add pre login layout

---
+++ frontend/layouts/preLogin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <the-header-pre-login />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheHeaderPreLogin from '../components/TheHeaderPreLogin.vue';
+
+export default {
+  components: {
+    TheHeaderPreLogin
+
+  },
+  head: {
+    bodyAttrs: {
+      class: "bg-white",
+    },
+  },
+};
+</script>
