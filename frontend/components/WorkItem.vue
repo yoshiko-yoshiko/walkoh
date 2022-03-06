cd4012c5cfef52dd332e298719b2f799119221f5
Keiichiro Sato <10kmartinis29@gmail.com>
2022-03-06 14:13:53 +0900

    Tweak styles

--- frontend/components/WorkItem.vue
+++ frontend/components/WorkItem.vue
@@ -1,14 +1,16 @@
 <template>
-  <div>
+  <div class="container justify-center text-center">
+    <div>
       <strong class="text-2xl">{{ $store.state.works.title }}</strong>
     </div>
-    <div>
+    <div class="mt-4">
       <p>
         <span>
           <b class="text-xl">&yen; {{ $store.state.works.price }}</b>
         </span>
       </p>
     </div>
+    <div class=" p-5 bg-primary-sub mt-10 text-primary-black rounded-lg">
       <p>{{ $store.state.works.description }}</p>
     </div>
   </div>
